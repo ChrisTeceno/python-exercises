bc2e49eadccb3b4bff522447b23e3fbe8c89cda7
Chris Teceno <chris.teceno@gmail.com>
2022-01-18 12:10:43 -0600

    add first notebook and python intro exercises

---
+++ python_introduction_exercises.py
@@ -0,0 +1,24 @@
+# Open Visual Studio Code and install the Python extension. This will 
+# provide nice editing features when writing Python code.
+
+# Ensure that you have the correct version of Python installed by running 
+# the command in this lesson that prints the version of Python that you have installed.
+
+# ➜  python-exercises python - v
+# Python 3.9.7 (default, Sep 16 2021, 08:50:36) 
+# [Clang 10.0.0 ] :: Anaconda, Inc. on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+
+# Create a hello world program. Create a text file named python_introduction_exercises.py 
+# and write a program that prints "Hello, World!" to the console. Run this program from 
+# the command line.
+
+# Using this file
+print("Hello, World!")
+
+# Inside of your hello world program, create a variable named greeting that contains
+#  the message that you will print to the console. Run your script interactively 
+# (with python -i python_introduction_exercises.py) and view the contents of the
+#  greeting variable.print ('hello world')
+
+greeting = ('hello world')
